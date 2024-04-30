35b32d5b084b7d913cbbaad0f43b9be919616c5b
Tomas Votruba <tomas.vot@gmail.com>
2024-04-30 02:11:56 +0200

    misc

--- bin/phpstan-bodyscan.php
+++ bin/phpstan-bodyscan.php
@@ -10,11 +10,6 @@ use TomasVotruba\ClassLeak\DependencyInjection\ContainerFactory;
 if (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
     // project's autoload
     require_once __DIR__ . '/../../../../vendor/autoload.php';
-}
-
-if (file_exists(__DIR__ . '/../vendor/scoper-autoload.php')) {
-    // A. build downgraded package
-    require_once __DIR__ . '/../vendor/scoper-autoload.php';
 } else {
     // B. local repository
     require_once __DIR__ . '/../vendor/autoload.php';
