4b1cb766d968afa4006fdc07985e470fe6a413d8
GitHub Action <action@github.com>
2024-05-06 11:09:58 +0000

    release PHP 7.2 downgraded

--- src/Logger.php
+++ src/Logger.php
@@ -1,7 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace TomasVotruba\PHPStanBodyscan;
 
 final class Logger
@@ -10,9 +9,8 @@ final class Logger
      * @var string
      */
     public const LOG_FILE_PATH = 'bodyscan-log.txt';
-
-    public static function log(string $message): void
+    public static function log(string $message) : void
     {
-        file_put_contents(self::LOG_FILE_PATH, $message . PHP_EOL . PHP_EOL, FILE_APPEND);
+        \file_put_contents(self::LOG_FILE_PATH, $message . \PHP_EOL . \PHP_EOL, \FILE_APPEND);
     }
 }
