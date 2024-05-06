bde854eb82b4d4f547c353555797658756ffc213
GitHub Action <action@github.com>
2024-05-06 14:39:46 +0000

    release PHP 7.2 downgraded

--- src/Utils/FileLoader.php
+++ src/Utils/FileLoader.php
@@ -1,37 +1,30 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace TomasVotruba\PHPStanBodyscan\Utils;
 
-use Webmozart\Assert\Assert;
-
+use PHPStanBodyscan202405\Webmozart\Assert\Assert;
 final class FileLoader
 {
     /**
      * @return array<string, string>
      */
-    public static function resolveEnvVariablesFromFile(string $envFile): array
+    public static function resolveEnvVariablesFromFile(string $envFile) : array
     {
         Assert::fileExists($envFile);
-
         // load env file
         /** @var string $envContent */
-        $envContent = file_get_contents($envFile);
-
-        $envLines = explode("\n", $envContent);
-
+        $envContent = \file_get_contents($envFile);
+        $envLines = \explode("\n", $envContent);
         // split by "="
         $envVariables = [];
         foreach ($envLines as $envLine) {
-            $envLineParts = explode('=', $envLine);
-            if (count($envLineParts) !== 2) {
+            $envLineParts = \explode('=', $envLine);
+            if (\count($envLineParts) !== 2) {
                 continue;
             }
-
             $envVariables[$envLineParts[0]] = $envLineParts[1];
         }
-
         return $envVariables;
     }
 }
