7b7330e81c98ff311d2ae0319e0daf8878fd0a88
GitHub Action <action@github.com>
2024-05-06 10:57:04 +0000

    release PHP 7.2 downgraded

--- src/Utils/JsonLoader.php
+++ src/Utils/JsonLoader.php
@@ -1,26 +1,21 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace TomasVotruba\PHPStanBodyscan\Utils;
 
 use JsonException;
-use Symfony\Component\Process\Process;
-
+use PHPStanBodyscan202405\Symfony\Component\Process\Process;
 final class JsonLoader
 {
     /**
      * @return array<string, mixed>
      */
-    public static function loadToArray(string $json, Process $process): array
+    public static function loadToArray(string $json, Process $process) : array
     {
         try {
-            return json_decode($json, true, 512, JSON_THROW_ON_ERROR);
+            return \json_decode($json, \true, 512, 0);
         } catch (JsonException $jsonException) {
-            throw new JsonException(sprintf(
-                'Could not decode JSON from phpstan: "%s"',
-                $json ?: $process->getErrorOutput()
-            ), 0, $jsonException);
+            throw new JsonException(\sprintf('Could not decode JSON from phpstan: "%s"', $json ?: $process->getErrorOutput()), 0, $jsonException);
         }
     }
 }
