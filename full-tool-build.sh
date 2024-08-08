18d7de58f7ad95c5bb8b9f46eaffb3e96fccfe1c
npo-mmenke <116382818+npo-mmenke@users.noreply.github.com>
2024-08-08 16:50:43 +0200

    add phpstan timeout option (#29)
    * add option for phpstan timeout
    
    * build script: suppress composer dev requirement hint and cleanup previous temp dir
    
    * fix nullable $phpstanTimeout

--- full-tool-build.sh
+++ full-tool-build.sh
@@ -7,8 +7,9 @@ composer install --ansi
 composer update --no-dev --ansi
 
 # downgrade with rector
+rm -rf rector-local
 mkdir rector-local
-composer require rector/rector --working-dir rector-local
+composer require rector/rector --working-dir rector-local --no-interaction
 rector-local/vendor/bin/rector process bin src vendor --config build/rector-downgrade-php-72.php --ansi
 
 # prefix
